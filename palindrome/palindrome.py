11ceab69a5acce83f944b5a3e7241653f31a3531
Niharika-JP <niharika07032001@gmail.com>
2024-11-13 17:21:33 -0500

    edited palindrome.py

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,4 +1,5 @@
 def is_palindrome(str_to_check):
- 
-  return False
+    filtered_chars = [char.lower() for char in str_to_check if char.isalnum()]
+    return filtered_chars == filtered_chars[::-1]
+
 
