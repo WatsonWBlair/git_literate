ebe3e7543d21b7460a582b96081188f1c66de6a5
zenil16 <zenil260301@gmail.com>
2024-11-13 17:27:30 -0500

    feat: all-construct complete!!

--- all-construct/all_construct.py
+++ all-construct/all_construct.py
@@ -3,4 +3,15 @@ def all_construct(target_word, word_bank):
     Returns an array containing all the ways that the `target` can be constructed
     by concatenating elements of the `word_bank` array
   '''
-  return target_word
+  if target_word == "":
+    return [[]]
+  all_ways = []
+
+  for word in word_bank:
+    if target_word.startswith(word):
+      suffix = target_word[len(word):]
+      suffix_ways = all_construct(suffix, word_bank)
+      target_ways = [[word] + way for way in suffix_ways]
+      all_ways.extend(target_ways)
+
+  return all_ways
