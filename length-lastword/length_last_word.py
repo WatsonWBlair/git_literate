5bc1333fc30aaec38e79417ebfd461aad3d6cba0
Martin Sichali <martinchunny@gmail.com>
2024-11-13 17:25:02 -0500

    length of last word challenge completed

--- length-lastword/length_last_word.py
+++ length-lastword/length_last_word.py
@@ -1,3 +1,7 @@
 def length_of_last_word(word):
   ''' Returns the length of the last word in a string '''
-  return True
+  # remove any trailing space and split 
+  words = word.strip().split()
+  # return the length of the last word 
+  return len(words[-1] if words else 0)
+ 
