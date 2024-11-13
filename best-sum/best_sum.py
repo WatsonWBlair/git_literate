d8c73c37956a9734f3ec3d814bbfc3a8fecc2f7e
System Administrator <root@SAIs-MacBook-Air-3.local>
2024-11-13 17:06:52 -0500

    Changes in best-sum

--- best-sum/best_sum.py
+++ best-sum/best_sum.py
@@ -1,4 +1,30 @@
-def best_sum(target_sum, numbers, memo = {}):
-  ''' Returns the shortest array of elements that add upto the target sum '''
-
-  return target_sum
+def best_sum(target_sum, numbers, memo=None):
+    if memo is None:
+        memo = {}
+    
+    # Base cases
+    if target_sum in memo:
+        return memo[target_sum]
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+    
+    shortest_combination = None
+    
+    # Recursive case: try each number in the list
+    for num in numbers:
+        remainder = target_sum - num
+        remainder_combination = best_sum(remainder, numbers, memo)
+        
+        # If we find a valid combination for the remainder
+        if remainder_combination is not None:
+            combination = remainder_combination + [num]
+            
+            # If it's shorter than the current shortest, update
+            if shortest_combination is None or len(combination) < len(shortest_combination):
+                shortest_combination = combination
+    
+    # Memoize the result for the target sum
+    memo[target_sum] = shortest_combination
+    return shortest_combination
