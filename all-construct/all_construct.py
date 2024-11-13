c5a815157aea8b5f5f76274b32058ce3226ca00c
Ruturaj Dixit <71138274+ruturajdixit99@users.noreply.github.com>
2024-11-13 16:14:56 -0500

    all-construct

--- all-construct/all_construct.py
+++ all-construct/all_construct.py
@@ -1,6 +1,19 @@
 def all_construct(target_word, word_bank):
-  '''
-    Returns an array containing all the ways that the `target` can be constructed
-    by concatenating elements of the `word_bank` array
-  '''
+  if target_word == "":
+        return [[]] 
+    
+  result = []
+    
+  for word in word_bank:
+    if target_word.startswith(word):
+      suffix = target_word[len(word):]  
+      suffix_ways = all_construct(suffix, word_bank)  
+      
+      target_ways = [[word] + way for way in suffix_ways]
+      result.extend(target_ways)
+
+    return result
+
+
+
   return target_word
